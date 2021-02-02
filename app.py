39540c8a76863b627f97b7828e426cf89ca70875
EC2 Default User <ec2-user@ip-172-31-15-207.ec2.internal>
2021-02-02 22:27:26 +0000

    Used methods from spotify,methods classes

--- app.py
+++ app.py
@@ -1,37 +1,37 @@
 from flask import Flask, render_template
-import os
 import random
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+import requests
+import os
 from dotenv import load_dotenv, find_dotenv
+from methods import get_access_token
+from spotify import get_header, get_top_10_tracks, get_artist_image, get_tracks_images, get_tracks_previews
 
 app = Flask(__name__)
-
-#abstraction of API keys
-load_dotenv(find_dotenv()) 
-
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
 @app.route('/')
 
 def hello_world():
+    access_token = get_access_token()
+    
+    header = get_header(access_token)
+    
     reference = ["Shawn Mendes", "BLACKPINK", "Panic! At The Disco", "Dua Lipa", "Zedd"]
     artists_id = ["7n2wHs1TKAczGzO7Dd2rGr", "41MozSoPIsD1dJM0CLPjZF", "20JZFwl6HVl6yg8a4H3ZqK", "6M2wZ9GZgrQXHCFfjv46we", "2qxJFvFYMEDqd7ui6kSAcq"]
     
     random_number = random.randint(0,4) 
-    artist_name = reference[random_number]
-    artist_img = spotify.search(q='artist:' + artist_name, type='artist')['artists']['items'][0]['images'][0]['url']
-    artist_id = artists_id[random_number]
+    random_track = random.randint(0,9)
     
-    random_track = random.randint(0,9) 
-    artist_top_10 = spotify.artist_top_tracks(artist_id, country="US")
-    tracks = []
-    tracks_preview_url = []
-    tracks_album_img = []
-    for i in range(10):
-        tracks.append(artist_top_10["tracks"][i]["name"])
-        tracks_preview_url.append(artist_top_10["tracks"][i]["preview_url"])
-        tracks_album_img.append(artist_top_10["tracks"][i]["album"]["images"][0]["url"])
+    artist_name = reference[random_number]
+    artist_id = artists_id[random_number]
+    artist_img = get_artist_image(artist_id, header)
+    
+    tracks = get_top_10_tracks(artist_id, header)
+    tracks_images = get_tracks_images(artist_id, header)
+    tracks_previews = get_tracks_previews(artist_id, header)
+    
+    print(tracks[random_track])
+    print(tracks_images[random_track])
+    print(tracks_previews[random_track])
     
     return render_template(
         "index.html",
@@ -40,8 +40,8 @@ def hello_world():
         tracks = tracks,
         len = len(tracks),
         track_name = tracks[random_track],
-        preview = tracks_preview_url[random_track],
-        track_image = tracks_album_img[random_track]
+        track_preview = tracks_previews[random_track],
+        track_image = tracks_images[random_track]
     )
     
 app.run(
