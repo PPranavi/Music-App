f84251ae6c8efec719f1f564024a77ee2a538716
EC2 Default User <ec2-user@ip-172-31-15-207.ec2.internal>
2021-02-09 16:29:43 +0000

    Established authorization header

--- genius.py
+++ genius.py
@@ -2,3 +2,13 @@ import requests
 import os
 from dotenv import load_dotenv, find_dotenv
 import requests
+
+BASE_URL = 'https://api.genius.com/'
+
+def get_header(access_token):
+    load_dotenv(find_dotenv())
+    token = os.getenv('GENIUS_ACCESS_TOKEN')
+    return {'Authorization': 'Bearer {token}'.format(token=token)}
+    
+def get_lyrics_link(lyrics_id, header):
+    return
